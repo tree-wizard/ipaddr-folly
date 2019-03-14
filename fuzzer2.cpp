f5807c71aed5be57cad13a71422a9b05d5495e8e
Xavier Cadena <xacadena@gmail.com>
2019-03-13 22:09:49 -0500

    Update fuzzer2.cpp

--- fuzzer2.cpp
+++ fuzzer2.cpp
@@ -2,7 +2,7 @@
 #include <folly/String.h>
 extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t size) {
 
-//folly::StringPiece bin1(data, size);
+folly::StringPiece bin1(data, size);
 folly::IPAddress::tryFromString(bin1);
 //folly::IPAddressV6::tryFromString(data);
 //folly::IPAddressV4::tryFromString(data);
