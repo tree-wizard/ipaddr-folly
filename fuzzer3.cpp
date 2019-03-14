cb7e7ab733c4bcb99ec93643bc5d1285a81e2036
Xavier Cadena <xacadena@gmail.com>
2019-03-13 22:10:25 -0500

    Update fuzzer3.cpp

--- fuzzer3.cpp
+++ fuzzer3.cpp
@@ -1,7 +1,8 @@
 #include <folly/IPAddress.h>
 extern "C" int LLVMFuzzerTestOneInput(const char *data, size_t size) {
 
-folly::IPAddress::validate(data);
+follyy::StringPiece bin1(data, size);
+folly::IPAddress::validate(bin1);
 // folly::IPAddressV4::validate(data);
 // folly::IPAddressV6::validate(data);
         return 0;
